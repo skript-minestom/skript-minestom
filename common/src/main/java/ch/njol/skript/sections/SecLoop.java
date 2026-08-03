10660482bacf73ef6f98de40b7db2d90b3d65fe0
hapily <87914807+hapily04@users.noreply.github.com>
2026-08-03 11:52:37 -0500

    optimize imports

--- common/src/main/java/ch/njol/skript/sections/SecLoop.java
+++ common/src/main/java/ch/njol/skript/sections/SecLoop.java
@@ -38,11 +38,9 @@ import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.WeakHashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Name("Loop")
