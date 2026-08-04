7fbe146f767f130ebd0e95271822db457a85482e
hapily <87914807+hapily04@users.noreply.github.com>
2026-08-04 10:59:26 -0500

    fix: formatting, syntax, and parseResult.mark usage

--- minestom/src/main/java/ch/njol/skript/expressions/ExprTPS.java
+++ minestom/src/main/java/ch/njol/skript/expressions/ExprTPS.java
@@ -1,10 +1,7 @@
 package ch.njol.skript.expressions;
 
 import ch.njol.skript.Skript;
-import ch.njol.skript.doc.Description;
-import ch.njol.skript.doc.Example;
-import ch.njol.skript.doc.Keywords;
-import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.*;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
@@ -16,15 +13,13 @@ import me.lucko.spark.api.statistic.types.DoubleStatistic;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Locale;
-
 @Name("TPS")
-@Description({
-	"The server's ticks per second, as measured by spark.",
-	"Without a window, this returns all five of spark's averages, in order: the last 5 seconds, 10 seconds, 1 minute, 5 minutes and 15 minutes."
-})
-@Example("broadcast \"%tps from the last 1 minute%\"")
-@Example("""
+@Description("""
+	The server's ticks per second, as measured by spark.
+	Without a window, this returns all five of spark's averages, in order: the last 5 seconds, 10 seconds, 1 minute, 5 minutes and 15 minutes.""")
+@Examples("""
+	broadcast "%tps from the last 1 minute%"
+	
 	every 30 seconds:
 		if tps from the last 5 seconds < 15:
 			broadcast "<red>The server is lagging!\"""")
@@ -33,7 +28,7 @@ public class ExprTPS extends SimpleExpression<Number> {
 
 	static {
 		Skript.registerExpression(ExprTPS.class, Number.class, ExpressionType.SIMPLE,
-			"[the] [server] tps [(from|over|in) [the] last (sec5:5 seconds|sec10:10 seconds|min1:1 minute|min5:5 minutes|min15:15 minutes)]");
+			"tps [(from|over|in) [the] last (1:5 seconds|2:10 seconds|3:1 minute|4:5 minutes|5:15 minutes)]");
 	}
 
 	@Nullable
@@ -41,8 +36,7 @@ public class ExprTPS extends SimpleExpression<Number> {
 
 	@Override
 	public boolean init(Expression<?>[] expressions, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
-		if (!parseResult.tags.isEmpty())
-			window = Window.valueOf(parseResult.tags.getFirst().toUpperCase(Locale.ENGLISH));
+		if (parseResult.mark != 0) window = Window.values()[parseResult.mark-1];
 		return true;
 	}
 
