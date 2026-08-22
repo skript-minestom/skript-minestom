7265a270d60c51c4205228d8f07cd2ea77cdec4d
hapily <87914807+hapily04@users.noreply.github.com>
2026-08-22 10:40:17 -0500

    feat: thread sleeping syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.github.hapily04.skriptminestom"
-version = "1.0.0-alpha.37"
+version = "1.0.0-alpha.38"
 
 repositories {
 	mavenCentral()
--- common/src/main/java/ch/njol/skript/effects/Delay.java
+++ common/src/main/java/ch/njol/skript/effects/Delay.java
@@ -17,34 +17,41 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.Set;
 import java.util.WeakHashMap;
 
 @Name("Delay")
-@Description("Delays the script's execution by a given timespan. Please note that delays are not persistent, e.g. trying to create a tempban script with <code>ban player → wait 7 days → unban player</code> will not work if you restart your server anytime within these 7 days. You also have to be careful even when using small delays!")
-@Examples({
-	"wait 2 minutes",
-	"halt for 5 minecraft hours",
-	"wait a tick"
-})
+@Description("""
+Delays the script's execution by a given timespan.
+Please note that delays are not persistent, e.g. trying to create a tempban script with <code>ban player → wait 7 days → unban player</code> will not work if you restart your server anytime within these 7 days.
+You also have to be careful even when using small delays!
+
+NOTE: Using sleep/pause will completely pause the thread this effect is run on. It's intended for pausing on another thread.""")
+@Examples("""
+	wait 2 minutes
+	halt for 5 minecraft hours
+	wait a tick
+	sleep for 2 ticks # completely pauses the thread this effect is on for 2 ticks""")
 @Since("1.4")
 public class Delay extends Effect {
 
 	static {
-		Skript.registerEffect(Delay.class, "(wait|halt) [for] %timespan%");
+		Skript.registerEffect(Delay.class, "(wait|halt) [for] %timespan%", "(sleep|pause) [thread] for %timespan%");
 	}
 
-	@SuppressWarnings("NotNullFieldNotInitialized")
 	protected Expression<Timespan> duration;
+
+	private int pattern;
 
 	@SuppressWarnings({"unchecked", "null"})
 	@Override
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		getParser().setHasDelayBefore(Kleenean.TRUE);
-
+		pattern = matchedPattern;
 		duration = (Expression<Timespan>) exprs[0];
-		if (duration instanceof Literal) { // If we can, do sanity check for delays
+		if (duration instanceof Literal && matchedPattern == 0) { // If we can, do sanity check for delays
 			Timespan timespan = ((Literal<Timespan>) duration).getSingle();
 			long millis = timespan.getAs(Timespan.TimePeriod.MILLISECOND);
 			if (millis < 50) {
@@ -58,15 +65,21 @@ public class Delay extends Effect {
 	@Override
 	@Nullable
 	protected TriggerItem walk(Event event) {
+		Timespan duration = this.duration.getSingle(event);
+		if (duration == null) return null;
+		if (pattern == 1) {
+			try {
+				Thread.sleep(duration.get(ChronoUnit.MILLIS));
+				return getNext();
+			} catch (InterruptedException _) {
+				return null;
+			}
+		}
 		debug(event, true);
 		long start = Skript.debug() ? System.nanoTime() : 0;
 		TriggerItem next = getNext();
 		if (next != null && Skript.getInstance().isEnabled()) { // See https://github.com/SkriptLang/Skript/issues/3702
 			addDelayedEvent(event);
-
-			Timespan duration = this.duration.getSingle(event);
-			if (duration == null)
-				return null;
 
 			// Back up local variables
 			Object localVars = Variables.removeLocals(event);
