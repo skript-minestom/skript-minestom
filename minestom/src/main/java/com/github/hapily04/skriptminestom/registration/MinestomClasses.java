59503bc49313e0dff4727f3522b2d670ec6c40bb
hapily <87914807+hapily04@users.noreply.github.com>
2026-08-25 10:27:31 -0500

    slight description change

--- minestom/src/main/java/com/github/hapily04/skriptminestom/registration/MinestomClasses.java
+++ minestom/src/main/java/com/github/hapily04/skriptminestom/registration/MinestomClasses.java
@@ -1213,9 +1213,7 @@ public class MinestomClasses {
 		Classes.registerClass(new EnumClassInfo<>(ItemFlag.class, "itemflag")
 			.user("item ?flags?")
 			.name("Item Flag")
-			.description("""
-				A part of an item's tooltip that can be hidden.
-				Possible values: hide enchants, hide attributes, hide unbreakable, hide destroys, hide placed on, hide dye, hide armor trim, hide stored enchants, hide additional tooltip.""")
+			.description("A part of an item's tooltip that can be hidden.")
 			.examples("""
 				give player diamond sword with the hide attributes item flag
 				add hide enchants to item flags of player's tool"""));
